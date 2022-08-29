2bfb5b1014164857b3bcb984c12f4fc68e965e3b
Mihai Panciu <panciu.rusu.mihai@gmail.com>
2022-08-29 21:40:50 +0300

    Add modals mock and change react port

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import './styles/global.scss';
+import { useState } from 'react';
+import styles from './App.module.scss';
+import Button from 'react-bootstrap/Button';
+import ModalA from './components/Modals/ModalA/ModalA';
+import ModalB from './components/Modals/ModalB/ModalB';
 
 function App() {
-  return <div className='App'></div>;
+  const [modalAShow, setModalAShow] = useState(false);
+  const [modalBShow, setModalBShow] = useState(false);
+
+  return (
+    <>
+      <div className={styles.container}>
+        <Button className={'m-1'} onClick={() => setModalAShow(true)}>
+          Button A
+        </Button>
+        <Button className={'m-1'} onClick={() => setModalBShow(true)}>
+          Button B
+        </Button>
+      </div>
+      <ModalA show={modalAShow} onHide={() => setModalAShow(false)} />
+      <ModalB show={modalBShow} onHide={() => setModalBShow(false)} />
+    </>
+  );
 }
 
 export default App;
